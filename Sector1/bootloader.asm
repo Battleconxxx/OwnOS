f1b559ef257be42a0818a732c2372e3688414a00
Battleconxxx <sudhanp2004@gmail.com>
2025-05-15 01:01:04 +0530

    Reading Disk into Memory

--- Sector1/bootloader.asm
+++ Sector1/bootloader.asm
@@ -1,15 +1,19 @@
 [org 0x7c00]
 
+mov [BOOT_DISK], dl
+
 mov bp, 0x7c00
 mov sp, bp
 
 mov bx, Teststring
 call Printfunction
 
+call ReadDisk
 
 JMP $
 
 %include "print.asm"
+%include "DiskRead.asm"
 
 times 510-($-$$) db 0
 
