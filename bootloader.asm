6f8340726802a58d3584a22571c3ebaf988b5c33
Battleconxxx <sudhanp2004@gmail.com>
2025-05-14 20:07:26 +0530

    commit2

--- bootloader.asm
+++ bootloader.asm
@@ -8,21 +8,8 @@ call Printfunction
 
 
 JMP $
-Printfunction:
-    mov ah, 0x0e
-    .Loop:
-        mov al, [bx]
-        int 0x10
-        inc bx
-        cmp [bx], byte 0
-        je .Exit
-        jmp .Loop
-    .Exit:
-        ret
 
-
-Teststring:
-    db 'This is a test string',0
+%include "print.asm"
 
 times 510-($-$$) db 0
 
